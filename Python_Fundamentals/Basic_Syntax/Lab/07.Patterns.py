c05d738ceefa573e0b74e77e23be4ee03e137a69
SlaveyaSV <slaveya.vladova@gmail.com>
2024-05-22 18:16:34 +0300

    Created 07.Patterns

---
+++ Python_Fundamentals/Basic_Syntax/Lab/07.Patterns.py
@@ -0,0 +1,21 @@
+n = int(input())
+
+for row in range(1, n+1):
+    print("*" * row)
+for row in range(n-1, 0, -1):
+    print("*" * row)
+
+
+# n = int(input())
+#
+# for row in range(1, n+1):
+#     for column in range(0, row):
+#         print("*", end="")
+#
+#     print()
+#
+# for row in reversed(range(1, n)):
+#     for column in range(0, row):
+#         print("*", end="")
+#
+#     print()
