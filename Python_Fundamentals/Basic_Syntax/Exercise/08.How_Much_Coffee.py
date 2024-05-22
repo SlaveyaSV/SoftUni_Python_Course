1fb5a35ac8ade22ccab2977bae927d7b664f9e1c
SlaveyaSV <slaveya.vladova@gmail.com>
2024-05-22 22:13:28 +0300

    Create 08.How_Much_Coffee.py

---
+++ Python_Fundamentals/Basic_Syntax/Exercise/08.How_Much_Coffee.py
@@ -0,0 +1,22 @@
+number_of_coffees = 0
+
+while True:
+    command = input()
+
+    if command == "END":
+        break
+
+    if (command.lower() == "coding" or
+            command.lower() == "dog" or
+            command.lower() == "cat" or
+            command.lower() == "movie"):
+        if command.islower():
+            number_of_coffees += 1
+        else:
+            number_of_coffees += 2
+
+if number_of_coffees > 5:
+    print("You need extra sleep")
+else:
+    print(number_of_coffees)
+
