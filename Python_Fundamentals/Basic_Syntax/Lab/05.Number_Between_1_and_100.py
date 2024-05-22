5f7999ce99caa2d8a87a3dc68acec5b74e0d9365
SlaveyaSV <slaveya.vladova@gmail.com>
2024-05-22 18:40:02 +0300

    Update 05.Number_Between_1_and_100.py

--- Python_Fundamentals/Basic_Syntax/Lab/05.Number_Between_1_and_100.py
+++ Python_Fundamentals/Basic_Syntax/Lab/05.Number_Between_1_and_100.py
@@ -4,11 +4,3 @@ while not 1 <= num <= 100:
     num = float(input())
 
 print(f"The number {num} is between 1 and 100")
-
-# while True:
-#     num = float(input())
-#
-#     if 1 <= num <= 100:
-#         break
-#
-# print(f"The number {num} is between 1 and 100")
