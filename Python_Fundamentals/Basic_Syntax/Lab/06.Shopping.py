0180bcd3c6553451c056d20a4f54dfafbd884018
SlaveyaSV <slaveya.vladova@gmail.com>
2024-05-22 18:28:52 +0300

    Update 06.Shopping.py

--- Python_Fundamentals/Basic_Syntax/Lab/06.Shopping.py
+++ Python_Fundamentals/Basic_Syntax/Lab/06.Shopping.py
@@ -14,20 +14,3 @@ while info != "End":
 
 else:
     print("You bought everything needed.")
-
-# budget = int(input())
-#
-# while True:
-#     command = input()
-#
-#     if command == "End":
-#         print("You bought everything needed.")
-#         break
-#     else:
-#         price = int(command)
-#
-#     if price <= budget:
-#         budget -= price
-#     else:
-#         print("You went in overdraft!")
-#         break
