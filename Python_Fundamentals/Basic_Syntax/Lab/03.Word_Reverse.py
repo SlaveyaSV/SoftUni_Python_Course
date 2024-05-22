a28704e3888d9c48ef8d9fe398c6ad6dc2e6a449
SlaveyaSV <slaveya.vladova@gmail.com>
2024-05-22 18:25:31 +0300

    Create 03.Word_Reverse.py

---
+++ Python_Fundamentals/Basic_Syntax/Lab/03.Word_Reverse.py
@@ -0,0 +1,11 @@
+word = input()
+print(word[::-1])
+
+# word = input()
+#
+# reversed_word = ""
+#
+# for i in reversed(range(0, len(word))):
+#     reversed_word += word[i]
+#
+# print(reversed_word)
