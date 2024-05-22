bb486a1df1e4b8d86a6128dec3a3c513bc1cd586
SlaveyaSV <slaveya.vladova@gmail.com>
2024-05-22 18:40:00 +0300

    Update 04.Even_Numbers.py

--- Python_Fundamentals/Basic_Syntax/Lab/04.Even_Numbers.py
+++ Python_Fundamentals/Basic_Syntax/Lab/04.Even_Numbers.py
@@ -9,17 +9,3 @@ for _ in range(n):
 
 else:
     print("All numbers are even.")
-
-# n = int(input())
-#
-# all_are_even = True
-#
-# for _ in range(n):
-#     num = int(input())
-#     if num % 2 != 0:
-#         print(f"{num} is odd!")
-#         all_are_even = False
-#         break
-#
-# if all_are_even:
-#     print("All numbers are even.")
