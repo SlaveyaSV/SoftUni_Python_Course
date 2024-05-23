a86a243c672a95b005dc56b6309e2c5472729fde
SlaveyaSV <slaveya.vladova@gmail.com>
2024-05-23 15:54:32 +0300

    Create 05.Special_Numbers.py

---
+++ Python_Fundamentals/02.Data_Types/Lab/05.Special_Numbers.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+for num in range(n, n+1):
+    digits_sum = 0
+
+    for digit in range(len(str(num))):
+        digits_sum += int(str(num)[digit])
+
+    if digits_sum == 5 or digits_sum == 7 or digits_sum == 11:
+        print(f"{num} -> True")
+    else:
+        print(f"{num} -> False")
